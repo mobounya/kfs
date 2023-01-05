9b84a0422b741fa6caf5d21169bcc86d16ef9960
Yahya EZ-ZAINABI <yezzainabi@gmail.com>
2023-01-05 21:05:15 +0000

    Display 42 as ascii art

--- kernel.cpp
+++ kernel.cpp
@@ -5,9 +5,21 @@
 
 extern "C" void kernel_main(void)
 {
-    const char *str = "42";
+    const char *str[] = {
+        "        :::      ::::::::",
+        "      :+:      :+:    :+:",
+        "    +:+ +:+         +:+  ",
+        "  +#+  +:+       +#+     ",
+        "+#+#+#+#+#+   +#+        ",
+        "     #+#    #+#          ",
+        "    ###   ########.fr    "
+    };
+    const size_t size = sizeof(str) / sizeof(const char *);
     VGA::TEXT_MODE vga_interface = VGA::TEXT_MODE();
 
-    vga_interface.set_start(VGA_HEIGHT / 2, (VGA_WIDTH / 2) - (ft_strlen(str) / 2));
-    vga_interface.write_string(str, VGA::BG_COLOR::BG_BLACK, VGA::FG_COLOR::RED, VGA::BLINK::TRUE);
+    for (size_t i = 0; i < size; ++i)
+    {
+        vga_interface.set_start((VGA_HEIGHT - size) / 2 + i, (VGA_WIDTH / 2) - (ft_strlen(str[i]) / 2));
+        vga_interface.write_string(str[i], VGA::BG_COLOR::BG_BLACK, VGA::FG_COLOR::RED, VGA::BLINK::TRUE);
+    }
 }
