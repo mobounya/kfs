3265eff662bf410b2f52893ecd3af4742daa85a3
Yahya EZ-ZAINABI <yezzainabi@gmail.com>
2023-01-27 12:49:45 +0000

    Mimic hexdump

--- not_libc.cpp
+++ not_libc.cpp
@@ -33,7 +33,22 @@ void dump_memory(VGA::TEXT_MODE &vga_interface, void *ptr, size_t size)
         print_hex(vga_interface, data[i]);
         ++i;
         if (i % 16 == 0)
+        {
+            size_t j = i - 16;
+            vga_interface.write_string("                ", VGA::BG_COLOR::BG_BLACK, VGA::FG_COLOR::BLACK, false);
+            while (j < i)
+            {
+                char str[2];
+                if (data[j] >= 32 && data[j] <= 126)
+                    str[0] = data[j];
+                else
+                    str[0] = '.';
+                str[1] = '\0';
+                vga_interface.write_string(str, VGA::BG_COLOR::BG_BLACK, VGA::FG_COLOR::GRAY, false);
+                j++;
+            }
             vga_interface.write_string("\n", VGA::BG_COLOR::BG_BLACK, VGA::FG_COLOR::BLACK, false);
+        }
         else
             vga_interface.write_string(" ", VGA::BG_COLOR::BG_BLACK, VGA::FG_COLOR::BLACK, false);
     }
