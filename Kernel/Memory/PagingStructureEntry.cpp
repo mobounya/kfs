17728a14aa79e0d7984d0d2a9d6062590e8d23b3
mobounya <amine.bounya20@gmail.com>
2023-03-01 16:45:31 +0000

    Kernel/Memory: implement null pointers.
    Made page 0x0 not present in the kernel virtual memory so derefrencing
    a NULL pointer will cause a page fault.

--- Kernel/Memory/PagingStructureEntry.cpp
+++ Kernel/Memory/PagingStructureEntry.cpp
@@ -2,58 +2,58 @@
 
 namespace Memory
 {
-    PagingStructureEntry *PagingStructureEntry::set_present(void)
+    PagingStructureEntry *PagingStructureEntry::set_present(bool value)
     {
-        this->present = 1;
+        this->present = value;
         return this;
     }
 
-    PagingStructureEntry *PagingStructureEntry::set_read_write(void)
+    PagingStructureEntry *PagingStructureEntry::set_read_write(bool value)
     {
-        this->read_write = 1;
+        this->read_write = value;
         return this;
     }
 
-    PagingStructureEntry *PagingStructureEntry::set_u_s(void)
+    PagingStructureEntry *PagingStructureEntry::set_u_s(bool value)
     {
-        this->u_s = 1;
+        this->u_s = value;
         return this;
     }
 
-    PagingStructureEntry *PagingStructureEntry::set_pwt(void)
+    PagingStructureEntry *PagingStructureEntry::set_pwt(bool value)
     {
-        this->pwt = 1;
+        this->pwt = value;
         return this;
     }
 
-    PagingStructureEntry *PagingStructureEntry::set_cache_disbled(void)
+    PagingStructureEntry *PagingStructureEntry::set_cache_disbled(bool value)
     {
-        this->cache_disbled = 1;
+        this->cache_disbled = value;
         return this;
     }
 
-    PagingStructureEntry *PagingStructureEntry::set_page_size(void)
+    PagingStructureEntry *PagingStructureEntry::set_page_size(bool value)
     {
-        this->pat = 1;
+        this->pat = value;
         return this;
     }
 
-    PagingStructureEntry *PagingStructureEntry::set_pat(void)
+    PagingStructureEntry *PagingStructureEntry::set_pat(bool value)
     {
-        this->pat = 1;
+        this->pat = value;
         return this;
     }
 
-    PagingStructureEntry *PagingStructureEntry::set_global(void)
+    PagingStructureEntry *PagingStructureEntry::set_global(bool value)
     {
-        this->global = 1;
+        this->global = value;
         return this;
     }
 
     PagingStructureEntry *PagingStructureEntry::set_physical_address(uint32_t physical_address)
     {
         // Address should be 4K page aligned.
-        this->page_table_address = physical_address >> 12;
+        this->physical_address = physical_address >> 12;
         return this;
     }
 }
